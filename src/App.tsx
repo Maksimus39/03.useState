c679643752d01adde6946e54ecb614cbd397c60c
Maksim <maksim.minakov.83@mail.ru>
2023-06-24 15:39:29 +0300

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-    //let a = 1
-
     let [a, setA] = useState(1)
-
-
     const onclickHandler = () => {
-        setA(++a);
+        setA(++a)
         console.log(a)
     }
-
-   // let [a, setA] =useState(0)
-    const onclickHandler0=()=>{
-        setA(0);
+    const onclickHandlerNoll = () => {
+      setA(0)
         console.log(a)
     }
-
+    const onclickHandlerMinus = () => {
+      setA(--a)
+        console.log(a)
+    }
 
     return (
-        <div className="App">
+        <div className={'App-header'}>
             <h1>{a}</h1>
             <button onClick={onclickHandler}>+</button>
-            <button onClick={onclickHandler0}>0</button>
+            <button onClick={onclickHandlerNoll}>0</button>
+            <button onClick={onclickHandlerMinus}>-</button>
         </div>
-    );
+    )
+
 }
 
 export default App;
